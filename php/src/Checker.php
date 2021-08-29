564a7df902319483a2e990a35cfcb2e8de28c61f
yukimasa ikeda <yyykms123@gmail.com>
2021-08-30 04:16:03 +0900

    feat: 三角形の判定処理とテストを追加

--- php/src/Checker.php
+++ php/src/Checker.php
@@ -9,7 +9,27 @@ class Checker
     // 引数と戻り値は変更してはいけない
     public static function apply(string $v): string
     {
-        // 実装はクラス・メソッド・例外など何をどう使っても良い
-        return '正三角形';
+        $reg = "/^([1-9]\d*|0)(\.\d+)? ([1-9]\d*|0)(\.\d+)? ([1-9]\d*|0)(\.\d+)?$/";
+        if (!preg_match($reg, $v)) {
+            // ３つの整数を半角スペース区切りで入力してください。
+            return '不成立';
+        }
+
+        $sides = explode(' ', $v);
+
+        if (in_array(0, $sides)) {
+            // 0を含んでいる
+            return '不成立';
+        }
+
+        $sameValues = array_count_values($sides);
+        $numberOfSameValues = count($sameValues);
+
+        return match ($numberOfSameValues) {
+            1 => "正三角形",
+            2 => "二等辺三角形",
+            3 => "不等辺三角形",
+            default => "不成立",
+        };
     }
 }
